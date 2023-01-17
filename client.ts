4001f59abe6e07898e364e1c6283b0da42bf9617
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2023-01-17 22:18:05 +0700

    Fix typo port between server and client

--- client.ts
+++ client.ts
@@ -1,4 +1,4 @@
 import * as grpc from '@grpc/grpc-js'
 import { UsersClient } from './src/stubs/protos/users_grpc_pb'
 
-export const client = new UsersClient('localhost:30000', grpc.credentials.createInsecure())
+export const client = new UsersClient('localhost:3000', grpc.credentials.createInsecure())
